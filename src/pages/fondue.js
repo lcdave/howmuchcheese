9a435404a359d170bbaf3a283cad2d8880cabc87
Davide Lo Cascio <davide@4eyes.ch>
2019-12-23 17:02:55 +0100

    renaming case sensitive

--- src/pages/fondue.js
+++ src/pages/fondue.js
@@ -1,17 +1,12 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import Counter from "../components/Counter";
-import {Helmet} from "react-helmet";
-import Header from "../components/Header";
+import Counter from "../components/counter";
+import Header from "../components/header";
 
 
 const Fondue = () => (
   <Layout>
-      <Helmet>
-          <link href="https://fonts.googleapis.com/css?family=Cookie&display=swap" rel="stylesheet" />
-          <script src="https://kit.fontawesome.com/f7f42ff28c.js" crossorigin="anonymous" />
-      </Helmet>
       <div className="lay_content">
           <Header showHeaderBack/>
         <Counter type="Fondue"/>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
-import Header from "../components/Header"
-import Card from "../components/Card"
+import Header from "../components/header"
+import Card from "../components/card"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
